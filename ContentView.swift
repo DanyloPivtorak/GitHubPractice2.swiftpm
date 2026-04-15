ce776df1be04047533aa12913f164495c4edfb2d
Louie2026 <boatfloor@icloud.com>
2026-04-15 11:08:51 -0500

    made it more pretty

--- ContentView.swift
+++ ContentView.swift
@@ -10,24 +10,41 @@ struct ContentView: View {
     var body: some View {
         TextField("", value: $number1, format: .number)
             .textFieldStyle(.roundedBorder)
+            .keyboardType(.numberPad)
         TextField("", value: $number2, format: .number)
             .textFieldStyle(.roundedBorder)
+            .keyboardType(.numberPad)
         
     
     Button ("add") {
         answer = number1 + number2
         }
+    .padding(5)
+    .background (Color.blue)
+    .foregroundColor(.white)
+    .cornerRadius(10)
+        
         Text("\(answer)")
         
         Button ("subtract") {
             subtractionanswer = number1 - number2
         }
+        .padding(5)
+        .background (Color.blue)
+        .foregroundColor(.white)
+        .cornerRadius(10)
+        
         Text("\(subtractionanswer)")
         
         Button ("multiply") {
             multianswer = Double (Int(number1 * number2))
         }
-        Text("\(multianswer)")
+        .padding(5)
+        .background (Color.blue)
+        .foregroundColor(.white)
+        .cornerRadius(10)
+        
+        Text("\(multianswer, specifier: "%.0f")")
     }
 }
 
