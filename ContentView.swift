08cd122ad30a7b929ab1521bc740effbb385541e
Danylo Pivtorak <dpivtorak9468@stu.d214.org>
2026-04-15 10:45:29 -0500

    added a textview

--- ContentView.swift
+++ ContentView.swift
@@ -3,9 +3,11 @@ import SwiftUI
 struct ContentView: View {
     
     @State var number1: Int = 0
-    
     @State var answer = 0
+    
     var body: some View {
         
+        Text("\(answer)")
     }
 }
+
